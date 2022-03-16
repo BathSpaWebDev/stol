da31ed4698da2c7c5c8ab17ca1d430642f000468
AndyFerrie <93442058+AndyFerrie@users.noreply.github.com>
2022-03-16 11:10:54 +0000

    fetch JSON

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React,{useState,useEffect} from 'react';
+
 import './App.css';
 
-const App = () => {
+function App() {
+  const [data,setData]=useState([]);
+  const getData=()=>{
+    fetch('db.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        console.log(response)
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        setData(myJson)
+      });
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
-    <div className="container">
-      Hello
+    <div className="App">
+     {
+       data && data.length>0 && data.map((item)=><p>{item.about}</p>)
+     }
     </div>
   );
 }
