f6a6707528cfdc6bcf8e54a4d6e2952fb8a004f5
Vladi <vsstanchev@gmail.com>
2022-03-16 13:23:38 +0000

    Add components & logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { Logo } from "../Logo/Logo";
+
+export const Header = () => {
+    return (
+
+      <header className="header">
+        <Logo />
+      <h1> Hello</h1>  
+      </header>
+    )
+}
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import logo from '../../logo.svg';
+
+export const Logo = () => {
+  console.log(logo)
+    return <img src={logo} className="App-logo" alt="logo"/>
+}
